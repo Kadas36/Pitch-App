581e0a48658254129c54441d4940ae7f8dbd2aa8
kadas36 <oduorthomas14@gmail.com>
2020-12-06 10:53:15 +0300

    Initialize application

--- app/__init__.py
+++ app/__init__.py
@@ -6,16 +6,16 @@ bootstrap = Bootstrap()
 
 def create_app(config_name):
 
-    app = Flask(__name__)
+   app = Flask(__name__)
 
-    # Creating the app configurations
-    app.config.from_object(config_options[config_name])
+   # Creating the app configurations
+   app.config.from_object(config_options[config_name])
 
-    #Initializing flask extensions
-    bootstrap.init_app(app)
+   #Initializing flask extensions
+   bootstrap.init_app(app)
 
-    #Registering the blueprint
-    from .main import main as main_blueprint
-    app.register_blueprint(main_blueprint)
+   #Registering the blueprint
+   from .main import main as main_blueprint
+   app.register_blueprint(main_blueprint)
 
-    return app
+   return app
