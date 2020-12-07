a787c553a675c96d45a7fe3b54740b7585a3a509
kadas36 <oduorthomas14@gmail.com>
2020-12-07 18:34:59 +0300

    Add static files which style pitch and index pages

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 export SECRET_KEY='mysecretkey101'
 # export MAIL_USERNAME= oduorthomas36@gmail.com
-# export MAIL_PASSWORD= kadaskale1
+# export MAIL_PASSWORD= kadaskale
 
 python3.6 manage.py server
