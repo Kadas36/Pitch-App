2f93286d3f5ad2f9c3ff4b50eed958169fabe242
kadas36 <oduorthomas14@gmail.com>
2020-12-07 13:50:05 +0300

    Add pitch and commet views

---
+++ app/models.py
@@ -0,0 +1,69 @@
+from . import db
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
+from . import login_manager
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+class User(UserMixin,db.Model):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key = True)
+    username = db.Column(db.String(255),index = True)
+    email = db.Column(db.String(255), unique = True, index = True)
+    pitch = db.relationship('Pitch', backref="user", lazy='dynamic')
+    comment = db.relationship('Comment', backref='main_user', lazy='dynamic')
+    pass_secure = db.Column(db.String(255))
+
+    @property
+    def password(self):
+        raise AttributeError('You cannnot read the password attribute')
+
+    @password.setter
+    def password(self, password):
+        self.pass_secure = generate_password_hash(password)
+
+
+    def verify_password(self,password):
+        return check_password_hash(self.pass_secure,password)
+
+    def __repr__(self):
+        return f'User {self.username}'
+
+
+class Category(db.Model):
+    __tablename__ = 'categories'
+
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String(255))
+    pitch = db.relationship('Pitch', backref='parent_category', lazy='dynamic')
+
+    def __repr__(self):
+        return f'Category {self.name}'
+
+class Pitch(db.Model):
+    __tablename__ = 'pitches'
+
+    id = db.Column(db.Integer, primary_key = True)
+    title = db.Column(db.String(255))
+    body = db.Column(db.String)
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"))
+    category_id = db.Column(db.Integer, db.ForeignKey("categories.id"))
+    comments = db.relationship('Comment', backref="main_pitch", cascade="all, delete-orphan" , lazy="dynamic")
+
+    def __repr__(self):
+        return f'Pitch {self.title}'
+
+class Comment(db.Model):
+    __tablename__ = 'comments'
+
+    id =  db.Column(db.Integer, primary_key = True)
+    author = db.Column(db.String(255))
+    comment = db.Column(db.String)
+    pitch_id = db.Column(db.Integer, db.ForeignKey("pitches.id"))
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"))
+
+    def __repr__(self):
+        return f'Comment {self.comment}'
