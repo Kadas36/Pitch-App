a7bf712e61004bad2addcafd4e174138cca34f02
kadas36 <oduorthomas14@gmail.com>
2020-12-07 04:38:20 +0300

    Add SQLAlchemy configuration

--- config.py
+++ config.py
@@ -1,12 +1,19 @@
 import os
 
 class Config:
+    
+   '''
+   General configuration parent class
+   '''
+   SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:kadas36@localhost/pitches'
+   SECRET_KEY = os.environ.get('SECRET_KEY')
 
-class prodConfig(config):
+class ProdConfig(Config):
     pass
 
-class DevConfig(config):
-    DEBUG = True
+class DevConfig(Config):
+   SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:kadas36@localhost/pitches'
+   DEBUG = True
 
 config_options = {
 'development':DevConfig,
